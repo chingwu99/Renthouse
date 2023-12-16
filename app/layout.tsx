0a560149d8fdaefea1415b8a68a690f194924cc0
chingwu99 <henry88028802@hotmail.com>
2023-12-16 20:10:40 +0800

    Fetching listings with server components (Listing card component, direct server action)

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,11 @@ export default async function RootLayout({
           <Navbar currentUser={currentUser} />
         </ClientOnly>
 
+        <div className="pb-20 pt-28">
         {children}
+        </div>
+
+      
       </body>
     </html>
   );
