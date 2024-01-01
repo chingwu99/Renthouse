37d29635b1414e1e94743a129188e91113ce5612
chingwu99 <henry88028802@hotmail.com>
2024-01-01 17:28:19 +0800

    feat: add filters modal (assigning various filters, add advanced querying logic to getListings, add loading and error pages)

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -29,17 +30,14 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
 
-        <div className="pb-20 pt-28">
-        {children}
-        </div>
-
-      
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
